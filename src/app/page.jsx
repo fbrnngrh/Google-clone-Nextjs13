8eecc231af837f2f1892e94d55c3e2db6435711d
Febrian Bayu Nugroho <93916449+fbrnngrh@users.noreply.github.com>
2023-04-12 20:30:06 +0800

    Add HomeHeader

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
+import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
   return (
-    <div className="text-3xl text-red-600 ">home</div>
-  )
+    <>
+      <HomeHeader />
+    </>
+  );
 }
