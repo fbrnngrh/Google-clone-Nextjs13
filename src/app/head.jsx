3f8843b2cb937bcdb8e7dafefc837533e00d065c
Febrian Bayu Nugroho <93916449+fbrnngrh@users.noreply.github.com>
2023-04-12 19:32:01 +0800

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> Google Clone with NextJS 13 </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Google Clone with NextJS 13" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
